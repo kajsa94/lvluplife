7604312c0dc14581f245def68b9c4c5bf22018dd
kajsa94 <76613061+kajsa94@users.noreply.github.com>
2025-10-21 00:55:48 +0200

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,15 @@
 <?php
-// Enkel manuell autoload för Google PHP Client
-require_once __DIR__ . '/google/src/Google/autoload.php';
-?>
+// Manuell autoload för Google API-klienten
+spl_autoload_register(function ($class) {
+    $prefix = 'Google\\';
+    $base_dir = __DIR__ . '/google/src/Google/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
