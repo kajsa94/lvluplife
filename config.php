55a6a4c024d1890acd983e2ec296cdc7f4317f66
kajsa94 <76613061+kajsa94@users.noreply.github.com>
2025-10-20 17:37:43 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// /web/lvluplife/auth/config.php
+$DB_HOST = "127.0.0.1";
+$DB_NAME = "lvlup";
+$DB_USER = "lvlupuser";
+$DB_PASS = "Drogtablettern!"; // byt om du ändrat
+
+try {
+  $pdo = new PDO(
+    "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
+    $DB_USER,
+    $DB_PASS,
+    [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]
+  );
+} catch (PDOException $e) {
+  http_response_code(500);
+  echo "DB error: " . htmlspecialchars($e->getMessage());
+  exit;
+}
+
+session_start();
